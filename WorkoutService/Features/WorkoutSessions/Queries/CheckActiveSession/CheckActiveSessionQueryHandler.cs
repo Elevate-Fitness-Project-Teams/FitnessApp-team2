9a89aa05cd103d6a29b4187a6d786144a009c50b
Mahmoud Emad <imahmoudemad8@gmail.com>
2026-07-07 06:51:00 +0300

    Refactor CheckActiveSessionQueryHandler

--- WorkoutService/Features/WorkoutSessions/Queries/CheckActiveSession/CheckActiveSessionQueryHandler.cs
+++ WorkoutService/Features/WorkoutSessions/Queries/CheckActiveSession/CheckActiveSessionQueryHandler.cs
@@ -8,25 +8,19 @@ namespace WorkoutService.Features.WorkoutSessions.Queries.CheckActiveSession;
 
 public class CheckActiveSessionQueryHandler : IRequestHandler<CheckActiveSessionQuery, Result>
 {
-    private readonly IGeneralRepo<WorkoutSession> _sessionRepo;
-	private readonly IUnitOfWork _unitOfWork;
-	public CheckActiveSessionQueryHandler(IGeneralRepo<WorkoutSession> sessionRepo, IUnitOfWork unitOfWork)
-    {
-        _sessionRepo = sessionRepo;
-        _unitOfWork = unitOfWork;
-    }
+	private readonly IGeneralRepo<WorkoutSession> _sessionRepo;
+	public CheckActiveSessionQueryHandler(IGeneralRepo<WorkoutSession> sessionRepo)
+	{
+		_sessionRepo = sessionRepo;
+	}
 
-    public async Task<Result> Handle(CheckActiveSessionQuery request, CancellationToken cancellationToken)
-    {
-		return await _unitOfWork.ExecuteAsync(async () =>
-        {
-            var activeSession = await _sessionRepo.GetAll()
-            .AnyAsync(s => s.UserId == request.UserId && s.Status == "Active", cancellationToken);
+	public async Task<Result> Handle(CheckActiveSessionQuery request, CancellationToken cancellationToken)
+	{
+		var activeSession = await _sessionRepo.GetAll()
+		.AnyAsync(s => s.UserId == request.UserId && s.Status == "Active", cancellationToken);
+		if (activeSession)
+			return Result.Failure(Error.Conflict(WorkoutErrorCodes.SessionAlreadyActive, "User already has an active workout session."));
 
-            if (activeSession)
-                return Result.Failure(Error.Conflict(WorkoutErrorCodes.SessionAlreadyActive, "User already has an active workout session."));
-
-            return Result.Success();
-        }, cancellationToken);
-    }
+		return Result.Success();
+	}
 }
