ddd43aab655033cc0e0eaa75911ff8b0fefa2204
Mahmoud Emad <imahmoudemad8@gmail.com>
2026-07-11 03:32:53 +0300

    Rethrow exceptions after logging in migration catch block.

--- WorkoutService/Program.cs
+++ WorkoutService/Program.cs
@@ -69,6 +69,7 @@ catch (Exception e)
 {
 	var logger = scopeApplicationContext.ServiceProvider.GetRequiredService<ILogger<Program>>();
 	logger.LogError(e, "An error occurred while migrating the database.");
+    throw;
 }
 
 #endregion
